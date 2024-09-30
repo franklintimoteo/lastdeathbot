749b0b251b850d852c8e3299d1d071cb0eab435a
Franklin Timóteo <franklindev@disroot.org>
2024-09-30 16:11:38 -0300

    Atualiza rsync para fazer um bypass ao enviar banco

--- db-rsync.sh
+++ db-rsync.sh
@@ -2,4 +2,4 @@ echo "Backup db"
 /lastdeath/backupdb.py
 
 echo "rsync database"
-rsync /tmp/backup.db ovh:/home/debian/lastdeath/deaths-database.sqlite
+rsync -e "ssh -o StrictHostKeyChecking=no" -arvc /tmp/backup.db ovh:/home/debian/lastdeath/deaths-database.sqlite
